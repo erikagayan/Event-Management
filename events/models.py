9e756d06a7c787a1a83de5b7bb4f431c2df0891e
Erik <erik.agayan.work@gmail.com>
2025-05-20 22:24:08 +0300

    Add new verbose names to field in models.py

--- events/models.py
+++ events/models.py
@@ -1,12 +1,20 @@
 from django.db import models
 from django.conf import settings
 
+
 class Event(models.Model):
+    """Model representing an event created by a user (organizer)."""
+
     title = models.CharField(max_length=100, verbose_name="Event name")
-    description = models.TextField()
-    date = models.DateTimeField()
-    location = models.CharField(max_length=250)
-    organizer = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    description = models.TextField(verbose_name="Description")
+    date = models.DateTimeField(verbose_name="Event date and time")
+    location = models.CharField(max_length=250, verbose_name="Location")
+    organizer = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        related_name = "events",
+        verbose_name = "Organizer"
+    )
 
     def __str__(self):
         return self.title
