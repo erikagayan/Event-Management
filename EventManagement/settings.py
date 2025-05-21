a0abe31eda05caddb47240a2f0ae1b61c43aec70
Erik <erik.agayan.work@gmail.com>
2025-05-22 01:22:16 +0300

    Add PostgreSQL

--- EventManagement/settings.py
+++ EventManagement/settings.py
@@ -1,6 +1,8 @@
 from datetime import timedelta
 from pathlib import Path
 
+from decouple import config
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -68,8 +70,12 @@ WSGI_APPLICATION = "EventManagement.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": config("DB_NAME"),
+        "USER": config("DB_USER"),
+        "PASSWORD": config("DB_PASSWORD"),
+        "HOST": config("DB_HOST"),
+        "PORT": config("DB_PORT"),
     }
 }
 
